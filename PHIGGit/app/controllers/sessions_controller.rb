cb266bf07aa4af1abf10e34995a3ec00509fcc0f
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-08-30 19:32:33 -0400

    Added sessions contorller, form & light styling to sessions page.

---
+++ PHIGGit/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path
+    else
+      flash[:error_message] = "Something went wrong. Please try again!"
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You've been logged out"
+    redirect_to root_path
+  end
+
+end
