2d0d18a9afe713aacf927328dc2ca928ee097555
Geoff <geoff.register@gmail.com>
2018-08-30 22:57:37 -0400

    user controller fleshed out a touch more

--- PHIGGit/app/controllers/users_controller.rb
+++ PHIGGit/app/controllers/users_controller.rb
@@ -1,17 +1,31 @@
 class UsersController < ApplicationController
+  
+  def index
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
  
-  @user.save
-  redirect_to @user
+    if
+      @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
-
-
-
-
-
-
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :username)
+    end
 
 
 end
