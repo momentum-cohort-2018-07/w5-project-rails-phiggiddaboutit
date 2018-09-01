9028d93e95190aa8f757357b3a146f76ad378495
Geoff <geoff.register@gmail.com>
2018-09-01 12:10:14 -0400

    removed user table columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_30_202947) do
+ActiveRecord::Schema.define(version: 2018_09_01_154309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,6 @@ ActiveRecord::Schema.define(version: 2018_08_30_202947) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.string "password"
-    t.string "password_confirmation"
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
