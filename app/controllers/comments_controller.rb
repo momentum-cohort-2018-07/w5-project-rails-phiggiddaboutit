c0a32fdbe9967a9ef2e78226dc9fb6221236feb7
gene-radcliffe <g_radcliffe@hotmail.com>
2018-09-01 19:19:38 -0400

    redoing feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,34 @@
 class CommentsController < ApplicationController
+  before_action :find_commentable
   def new
+    @comment = Comment.new
   end
 
+  def create
+      @comment= @commentable.comments.new comment_params
+      if @comment.save then
+        redirect_to story_path(1) 
+      else
+        redirect_to story_path(1)
+      end
+  end
   def destroy
   end
 
   def show
+    
   end
 
   def edit
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
+  def find_commentable
+    @commentable= Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+    @commentable = Story.find_by_id(params[:story_id]) if params[:story_id]
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,6 @@ class Comment < ApplicationRecord
 =begin
 Hello Gene!! 
 =end 
-
+belongs_to :commentable, polymorphic: true
+  has_many :comments, as: :commentable
 end
