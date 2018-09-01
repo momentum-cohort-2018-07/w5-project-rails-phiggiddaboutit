788feddb1a0718ef85cd9b0786f763553293dd75
gene-radcliffe <g_radcliffe@hotmail.com>
2018-09-01 14:54:17 -0400

    commit partially working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,17 @@
 class CommentsController < ApplicationController
+  before_action :find_commentable
   def new
+    @comment = Comment.new
   end
 
+  def create
+      @comment= @commentable.comments.new comment_params
+      if @comment.save then
+        redirect_to :back, notice: 'Your comment was successfully posted!'
+      else
+        redirect_to :back, notice: 'Your comment was not posted!'  
+      end
+  end
   def destroy
   end
 
@@ -10,5 +20,15 @@ class CommentsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
+  def find_commentable
+    @commentable= Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+    @commentable = Story.find_by_id(params[:story_id]) if params[:story_id]
   end
 end
